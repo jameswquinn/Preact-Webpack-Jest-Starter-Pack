f81d234f1b2029eaaf3640de0c0e9ca6687fddec
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 22:55:29 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,11 @@
 import { h } from 'preact';
 import { HelloWorld } from './HelloWorld';
-import responsiveImage from '../../public/responsive-image.png';
 
 export function App() {
   return (
     <div class="app">
       <h1>Preact Webpack Jest Starter Pack</h1>
       <HelloWorld />
-      <div class="responsive-image-container">
-        <img
-          src={responsiveImage.src}
-          srcSet={responsiveImage.srcSet}
-          sizes="(max-width: 600px) 300px, (max-width: 1200px) 600px, 900px"
-          alt="Responsive Image"
-          loading="lazy"
-        />
-      </div>
     </div>
   );
 }
