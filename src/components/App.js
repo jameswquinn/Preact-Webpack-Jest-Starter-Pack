779ded699cdb1f4487763104b3cdada04be98862
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 23:01:21 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import { h } from 'preact';
 import { HelloWorld } from './HelloWorld';
+import responsiveImage from '../../public/responsive-image.png';
 
 export function App() {
   return (
     <div class="app">
       <h1>Preact Webpack Jest Starter Pack</h1>
       <HelloWorld />
+      <div class="responsive-image-container">
+        <img
+          src={responsiveImage}
+          alt="Responsive Image"
+          loading="lazy"
+        />
+      </div>
     </div>
   );
 }
